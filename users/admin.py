5d7867a98bf1576d9740626732d26a12d803775d
ShutenkoStas <shotenko494@gmail.com>
2026-02-09 19:13:50 +0000

    Add models Role, UserRole, Poll, PollOption, Vote, Portfolio, PortfolioFile, also add to admin panel User, Poll, Portfolio, Vote

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import Poll, User, Portfolio, Vote
+
+admin.site.register(User)
+admin.site.register(Poll)
+admin.site.register(Portfolio)
+admin.site.register(Vote)
 
 # Register your models here.
--- users/models.py
+++ users/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class UserRole(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    role_id = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+
+class Poll(models.Model):
+    topic = models.CharField(max_length=128)
+    description = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class PollOption(models.Model):
+    poll_id = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    text = models.TextField()
+
+class Vote(models.Model):
+    poll_id = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    poll_option = models.CharField(max_length=128)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Mate:
+        unique_together = ("poll_id", "user_id")
+    
+class Portfolio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class PortfolioFile(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10)
+    file = models.FileField(null=True)
+    url = models.CharField(max_length=100, null=True)
