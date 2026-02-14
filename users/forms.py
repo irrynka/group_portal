8659f7f68bc511a7b58f277846d176d134a8b507
ShutenkoStas <shotenko494@gmail.com>
2026-02-14 20:47:19 +0000

    Add form AddPoll for page of adding poll, add poll and poll and add_poll templates, connect bootstrap for JS and CSS to base html, add sample poll_list, poll_detail, poll_create view

---
+++ users/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from users import models
+
+class AddPoll(forms.ModelForm):
+    option1 = forms.CharField(
+                            max_length=128,
+                            label="Варіант 1",
+                            required= True,
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введіть варіант 1'}))
+    option1 = forms.CharField(
+                            max_length=128,
+                            label="Варіант 2",
+                            required= True,
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введіть варіант 2'}))
+    option1 = forms.CharField(
+                            max_length=128,
+                            label="Варіант 3",
+                            required=False,
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введіть варіант 3'}))
+    option1 = forms.CharField( 
+                            max_length=128,
+                            label="Варіант 4",
+                            required=False,
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введіть варіант 4'}))
+    class Meta:
+        model = models.Poll
+        fields = ["text"]
+
+    def __init__(self, *args, **kwargs):
+        super(AddPoll, self).__init__(*args, **kwargs)
+
+        self.fields['text'].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Назва"
+        })
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, View, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from users import models
+
+class poll_list(ListView):
+    model = models.Poll
+    context_object_name = "Polls"
+    template_name = "users/poll.html"
+
+class poll_detail(LoginRequiredMixin ,DetailView):
+    model = models.Poll
+    context_object_name = "Poll_deteil"
+    template_name = "users/poll.html"
+
+class poll_create(LoginRequiredMixin, CreateView):
+    model = models.Poll
+    context_object_name = "Poll_create"
+    template_name = "users/poll.html"
+
+
+
+
+    
 
 # Create your views here.
