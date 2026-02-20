95d9dc3e654a720916cdb84618cc37888ada7874
ShutenkoStas <shotenko494@gmail.com>
2026-02-20 22:54:52 +0000

    -Fix error when url path for poll_add dont see poll_create view bc it commented so url for poll_add also comment

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from users import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("poll/", views.poll_list.as_view(), name="poll_list"),
-    path("poll_add/", views.poll_create.as_view(), name="poll_add"),
+    # path("poll_add/", views.poll_create.as_view(), name="poll_add"),
 
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", views.CustomLogoutView.as_view(), name="logout"),
